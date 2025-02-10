0ffddfc213e2f005258f7f70f30f28188ecf60ee
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-10 03:48:15 +0300

    Add files via upload

---
+++ prac20.2/PersonalComputer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac20._2
+{
+    internal class PersonalComputer : Program
+    {
+        public string Model;
+        public double CpuFrequency;
+        public int RamSize;
+        public int HddSize;
+        public PersonalComputer()
+        {
+            Model = "Неизвестно";
+            CpuFrequency = 0;
+            RamSize = 0;
+            HddSize = 0;
+        }
+        public PersonalComputer(string model)
+        {
+            Model = model;
+            CpuFrequency = 0;
+            RamSize = 0;
+            HddSize = 0;
+        }
+        public PersonalComputer(string model, double cpuFrequency)
+        {
+            Model = model;
+            CpuFrequency = cpuFrequency;
+            RamSize = 0;
+            HddSize = 0;
+        }
+        public PersonalComputer(string model, double cpuFrequency, int ramSize)
+        {
+            Model = model;
+            CpuFrequency = cpuFrequency;
+            RamSize = ramSize;
+            HddSize = 0;
+        }
+        public PersonalComputer(string model, double cpuFrequency, int ramSize, int hddSize)
+        {
+            Model = model;
+            CpuFrequency = cpuFrequency;
+            RamSize = ramSize;
+            HddSize = hddSize;
+        }
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Частота процессора: {CpuFrequency} ГГц");
+            Console.WriteLine($"Оперативная память: {RamSize} ГБ");
+            Console.WriteLine($"Жесткий диск: {HddSize} ГБ");
+        }
+        public override string ToString()
+        {
+            return $"Модель: {Model}, Процессор: {CpuFrequency} ГГц, ОЗУ: {RamSize} ГБ, HDD: {HddSize} ГБ";
+        }
+
+
+
+
+    }
+}
---
+++ prac20.2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac20._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalComputer pc1 = new PersonalComputer();
+            pc1.Model = "PC1";
+            pc1.CpuFrequency = 3.0;
+            pc1.RamSize = 16;
+            pc1.HddSize = 512;
+            pc1.Info();
+            Console.WriteLine(pc1.ToString());
+            Console.WriteLine();
+
+            PersonalComputer pc2 = new PersonalComputer("PC2");
+            pc2.CpuFrequency = 2.8;
+            pc2.RamSize = 8;
+            pc2.HddSize = 256;
+            pc2.Info();
+            Console.WriteLine(pc2.ToString());
+            Console.WriteLine();
+
+            PersonalComputer pc3 = new PersonalComputer("PC3", 4.0);
+            pc3.RamSize = 32;
+            pc3.HddSize = 1024;
+            pc3.Info();
+            Console.WriteLine(pc3.ToString());
+            Console.WriteLine();
+
+            PersonalComputer pc4 = new PersonalComputer("PC4", 3.2, 16);
+            pc4.HddSize = 512;
+            pc4.Info();
+            Console.WriteLine(pc4.ToString());
+            Console.WriteLine();
+
+            PersonalComputer pc5 = new PersonalComputer("PC5", 4.5, 64, 2048);
+            pc5.Info();
+            Console.WriteLine(pc5.ToString());
+            Console.WriteLine();
+
+            Console.Write("Введите модель ПК: ");
+            string model = Console.ReadLine();
+            Console.Write("Введите тактовую частоту процессора (в ГГц): ");
+            double cpuFrequency = double.Parse(Console.ReadLine());
+            Console.Write("Введите объем оперативной памяти (в ГБ): ");
+            int ramSize = int.Parse(Console.ReadLine());
+            Console.Write("Введите объем жесткого диска (в ГБ): ");
+            int hddSize = int.Parse(Console.ReadLine());
+            PersonalComputer pc6 = new PersonalComputer(model, cpuFrequency, ramSize, hddSize);
+            pc6.Info();
+            Console.WriteLine(pc6.ToString());
+
+
+
+
+
+
+
+        }
+    }
+}
