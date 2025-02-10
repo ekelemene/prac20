b0661a43d85832549afcd3a2bf6619f247430906
ekelemene <ksuxaskrip2007@gmail.com>
2025-02-10 03:43:56 +0300

    Add files via upload

---
+++ prac20/Build.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace prac20
+{
+    internal class Build : Program
+    {
+        public string name;
+        public double area;
+        public int kvo;
+        public int floor;
+
+        public Build()
+        {
+            name = "Неизвестно";
+            area = 0;
+            kvo = 0;
+            floor = 0;
+        }
+        public Build(string name)
+        {
+            this.name = name;
+
+        }
+        public Build(string name, double area)
+        {
+            this.name = name;
+            this.area = area;
+
+        }
+        public Build(string name, double area, int kvo)
+        {
+            this.name = name;
+            this.area = area;
+            this.kvo = kvo;
+        }
+        public Build(string name, double area, int kvo, int floor)
+        {
+            this.name = name;
+            this.area = area;
+            this.kvo = kvo;
+            this.floor = floor;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название строения: {name}");
+            Console.WriteLine($"Площадь строения: {area} м3 ");
+            Console.WriteLine($"Количество жильцов: {kvo}");
+            if (kvo > 0)
+            {
+                double areaPerP = area / kvo;
+                Console.WriteLine($"Площадь на одного жильца: {areaPerP:F2} м2");
+            }
+            else
+            {
+                Console.WriteLine("Нет жильцов для расчета площади на одного.");
+            }
+        }
+
+
+
+    }
+}
---
+++ prac20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prac20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Build dom1 = new Build();
+            dom1.name = "Жилой дом 1";
+            dom1.area = 150.5;
+            dom1.kvo = 5;
+            dom1.ShowInfo();
+            Console.WriteLine();
+
+            Build dom2 = new Build("отель", 6000, 1250);
+            dom2.ShowInfo();
+            Console.WriteLine();
+
+            Console.Write("Название строения: ");
+            string name = Console.ReadLine();
+            Console.Write("Площадь строения: ");
+            double area = double.Parse(Console.ReadLine());
+            Console.Write("Количество жильцов: ");
+            int kvo = int.Parse(Console.ReadLine());
+            Console.Write("Количество этажей: ");
+            int floor = int.Parse(Console.ReadLine());
+            Build dom3 = new Build(name, area, kvo, floor);
+            Console.WriteLine("dom3:");
+            dom3.ShowInfo();
+
+
+
+
+        }
+    }
+}
